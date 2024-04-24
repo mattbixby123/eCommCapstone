bfb9af271558331c00ec9f8dc133e7cf4c26cc08
Matthew Bixby <matthewbixby@matthews-air.myfiosgateway.com>
2024-04-24 19:54:19 -0400

    dependencies installed for react/redux/toolkit etc

--- src/client/main.jsx
+++ src/client/main.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './style.css';
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import store from "./redux/store";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
