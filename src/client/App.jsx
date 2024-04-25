b969f59fb5690adb27701f405d99103ae2a98c1d
Matthew Bixby <matthewbixby@matthews-air.myfiosgateway.com>
2024-04-25 11:27:54 -0400

    removed old code and logo import from app.jsx

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-// import reactLogo from './assets/react.svg';
 // import Login from './components/Login';
 import { Routes, Route } from 'react-router-dom'
 import Welcome from '../client/components/Welcome'
@@ -22,12 +21,6 @@ function App() {
         </Routes>
         </div>
     </>
-    // <div className='App'>
-    //     <h1>Boilerplate</h1>
-    //     <img id='comp-img' src='./computer.png'></img>
-    //     <p>Replace the starter code in this template with something cool</p>
-    //     <Login />
-    // </div>
   );
 }
 
