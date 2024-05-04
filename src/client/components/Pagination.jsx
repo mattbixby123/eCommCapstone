89e7cb6eb42822bc1deaca5322e71b8d764462ce
Matthew Bixby <matthewbixby@matthews-air.myfiosgateway.com>
2024-05-04 10:34:01 -0400

    pages arent changing, but the pagination component is finally showing up... just not effecting the paginated items on the screen when clicked. but the buttons are responsive

--- src/client/components/Pagination.jsx
+++ src/client/components/Pagination.jsx
@@ -1,37 +1,64 @@
 // Pagination.js
 // work in progress - pagination seems to be working but cannot get the react component to render and allow us to change pages.
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 
 
-const Pagination = ({ dataType }) => {
-  const [page, setPage] = useState(0);
+const Pagination = (endpoint, page) => {
+  const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(0);
   const [results, setResults] = useState([]);
 
-  useEffect(() => {
-    dataType(totalPages, page)
-      .then(({ data }) => {
-        setResults(data.results);
-        setTotalPages(data.total);
-      })
-      .catch((err) => console.log(err))
-      .finally(() => setLoading(false));
-  }, [page]);
+  const fetchResults = async (endpoint, page) => {
+    try {
+      const response = await axios.get(`api/product/${endpoint}?page=${page}&pageSize=10`);
+      const { results, totalPages } = response.data;
+      setResults(results);
+      setTotalPages(totalPages);
+    } catch (error) {
+       console.log(error);
+    }
+  };
   
-  const handleNextPage = () => {
-    if (page < totalPages) {
-      setPage(page + 1);
-    }
-  };
-
+  useEffect(() => {
+    fetchResults('comics', currentPage); // For comics
+  }, [currentPage]);
+  
+  useEffect(() => {
+    fetchResults('books', currentPage); // For books
+  }, [currentPage]);
+  
+  useEffect(() => {
+    fetchResults('magazines', currentPage); // For magazines
+  }, [currentPage]);
+  
+  
   const handlePrevPage = () => {
-    if (page > 1) {
-      setPage(page - 1);
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
     }
   };
 
-  return { results, totalPages, handleNextPage, handlePrevPage }
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+
+  return (
+    <div>
+            {/* Pagination controls */}
+      <button onClick={handlePrevPage} disabled={currentPage === 1}>
+        Previous Page
+      </button>
+      <button onClick={handleNextPage} disabled={currentPage === totalPages}>
+        Next Page
+      </button>
+    </div>
+  );
+  
 };
 
 export default Pagination;
