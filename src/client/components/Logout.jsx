1d2a4ad2d92e27443c853696df68635835bd04c7
sjswan13 <sjswan13@gmail.com>
2024-05-09 14:39:15 -0600

    slight changes with css and logout functionality

--- src/client/components/Logout.jsx
+++ src/client/components/Logout.jsx
@@ -2,6 +2,7 @@ import { useDispatch } from "react-redux";
 import { clearToken } from '../redux/authslice';
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
+import { useLogoutMutation } from '../redux/api'
 
 const useLogout = () => {
   const dispatch = useDispatch();
