43a368f4db02ab484bf9d4ab195044216f1b0b6e
Matthew Krichmar <matthewkrichmar@matthews-air.myfiosgateway.com>
2024-05-14 15:26:23 -0400

    stripe working with dummy data

--- src/client/components/Cart.jsx
+++ src/client/components/Cart.jsx
@@ -24,7 +24,25 @@ const Cart = () => {
   };
   
   const handleProceedToCheckout = () => {
-    dispatch(proceedToCheckout());
+    fetch('api/checkout/create-checkout-session', {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        items: [
+          { id: 1, quantity: 2 },
+          { id: 2, quantity: 1 }
+        ]
+      })
+    }).then(res => {
+      if (res.ok) return res.json()
+    }).then(({ url }) => {
+      window.location = url
+    }).catch(e => {
+      console.error(e.error);
+    })
+    // dispatch(proceedToCheckout());
   };
 
 
