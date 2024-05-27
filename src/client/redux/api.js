ce7752ad80078354dd43794c5ac43395973cb78d
Matthew Bixby <matthewbixby@matthews-air.myfiosgateway.com>
2024-05-27 17:32:54 -0400

    get a working dev build for trying solution for single product view

--- src/client/redux/api.js
+++ src/client/redux/api.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const api = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: process.env.SERVER_URL | 'http://localhost:3000/',
+    baseUrl: 'http://localhost:3000/',
     prepareHeaders: (headers, { getState }) => {
       const token = getState().auth.token;
       console.log(getState());
--- src/server/api/checkout.js
+++ src/server/api/checkout.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+// require('dotenv').config();
 const router = require("express").Router();
 const express = require('express');
 const app = express();
