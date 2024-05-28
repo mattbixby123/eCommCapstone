a78b494afe54be1695c3dcd3896d4dd952e86ccb
sjswan13 <sjswan13@gmail.com>
2024-05-28 10:42:45 -0600

    new changes with basURL adjust

--- src/client/redux/api.js
+++ src/client/redux/api.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const api = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:3000/',
+    baseUrl: process.env.SERVER_URL,
     prepareHeaders: (headers, { getState }) => {
       const token = getState().auth.token;
       console.log(getState());
